88a321eddc02923545473f5e15f3790cae21826d
Agaba-Ed <agabsed@gmail.com>
2021-08-02 13:02:28 +0300

    Uploaded starter files

---
+++ app.js
@@ -0,0 +1,53 @@
+const parser=require('node-c-parser');
+const express=require('express');
+const bodyParser=require('body-parser');
+const fs=require('fs');
+
+var app=express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000,function(err){
+    if(err){
+        console.log(err);}
+    else
+        console.log('server is running at port 3000');
+});
+
+
+//serving static files
+app.use(express.static(__dirname+'/public'));
+
+//handling post requests
+app.post( '/analyse',(request,response)=>{
+    const data=request.body;
+    fs.writeFile(__dirname+'/ed.c',data.codeText,err=>{
+        if(err){
+            console.log('error');
+        }
+        else{
+            console.log('File uploaded to server successfully');
+        }
+    });
+    parser.lexer.cppUnit.clearPreprocessors(
+        "./ed.c",(err,codeText)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                var tokens = parser.lexer.lexUnit.tokenize(codeText);
+                var dataToSend={
+                    "tokens":tokens
+                }
+                response.send(dataToSend);
+
+            }
+        }
+    );
+});
+
+
+
+
+
+
